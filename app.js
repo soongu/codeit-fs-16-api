3ae01c96f8b18bffa538d25067ed9834501dbe01
soongu <naong526@naver.com>
2026-09-17 13:03:27 +0900

    feat: add POST endpoint for creating new posts and update request examples

--- app.js
+++ app.js
@@ -2,7 +2,12 @@
 import express from 'express';
 import { posts } from './data/posts.js';
 
+let nextId = 4;
+
 const app = express();
+
+// 모든 요청 초입에 작동해서 클라이언트가 보낸 json을 재조립
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('인스타그램 서버가 살아 있어요');
@@ -26,6 +31,24 @@ app.get('/api/posts/:id', (req, res) => {
 
   res.json(post);
 });
+
+app.post('/api/posts', (req, res) => {
+
+  // 실제로 게시물을 추가해 줘야함.
+  const newPost = {
+    ...req.body,
+    id: nextId,
+    likeCount: 0,
+    commentCount: 0,
+    minutesAgo: 0,
+  };
+
+  nextId++;
+  posts.push(newPost);
+
+  res.status(201).json(newPost);
+});
+
 
 // 404 처리를 기본설정에서 커스텀설정으로 변경
 app.use((req, res) => {
